c1c71d73919c097e5d360933490be94b1c919b62
hanjiedeng <apple@AppledeMacBook-Pro.local>
2023-07-21 09:58:14 +0800

    Stage2 start

---
+++ program/func_public.py
@@ -0,0 +1,15 @@
+from constants import RESOLUTION
+from func_utils import get_ISO_times
+import pandas as pd
+import numpy as np
+import time
+
+from pprint import pprint
+
+ISO_TIMES = get_ISO_times()
+
+pprint(ISO_TIMES)
+
+# Construct market prices
+def construct_market_prices(client):
+    pass
